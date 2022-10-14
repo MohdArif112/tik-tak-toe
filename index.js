48dbf0e9c839308df313756914a3f59816d599e9
MohdArif <arif112mohd@gmail.com>
2022-10-14 10:26:39 +0530

    adding sound effect

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let winDiv= document.getElementsByClassName('popup')[0];
 let popupHeading = document.getElementById("popupHeading");
 let popupConfirnBtn = document.getElementById("popupBtnConfirm");
 let popupRejectBtn = document.getElementById("popupBtnReject");
+var audio= new Audio("success-fanfare-trumpets-6185.mp3");
+var correctAudio= new Audio("correct.mp3")
+var IncorrectAudio=  new Audio("surprised-child.mp3");
 let count = 0;
 let xWins = false;
 let player1 ;
@@ -29,11 +32,11 @@ function winpop(){
 
    
     //console.log(player1.value);
-     popupHeading.innerText="Congratulations" + "  " +player1.value;
+     popupHeading.innerText= "Winner"+"    " +player1.value;
     //  popupHeading2.innerText= "Dou You Want To continue";
   }
   else{
-    popupHeading.innerText="Congratulations" +"  " + player2.value;
+    popupHeading.innerText= "Winner"+"     " +player2.value;
   }
 }
 
@@ -57,23 +60,31 @@ function winGame(){
   wins.forEach(element => {
     console.log("win game called");
     if((colEle[element[0]].innerText === colEle[element[1]].innerText) && (colEle[element[2]].innerText === colEle[element[1]].innerText) && (colEle[element[0]].innerText !== "")){
+
+      audio.play();
+
+
+      colEle[element[0]].style.color="red";
+      colEle[element[1]].style.color="red";
+      colEle[element[2]].style.color="red";
+
+
       if(colEle[element[1]].innerText==='X'){
+
+       
         setTimeout(() => {
           xWins= true;
 
-          setTimeout(() => {
-            
-            winpop();
-          }, 1000);
+          winpop();
           
-        },500);
+        },1000);
 
       }
       else{
         setTimeout(() => {
           
           winpop();
-        }, 500);
+        }, 1000);
       }
     }
     
@@ -110,22 +121,14 @@ function generateGameGrid() {
 
   player1 = document.createElement("input");
   player1.setAttribute("type", "text");
-  player1.style.height = "40px";
-  player1.style.width = "250px";
-  player1.style.margin = "10px";
-  player1.style.padding = "10px";
-  player1.style.borderRadius = "50px";
-  player1.style.fontSize = "20px";
+  player1.classList.add("playerInput");
+  
   player1.setAttribute("placeholder", "First Player");
 
   player2 = document.createElement("input");
   player2.setAttribute("type", "text");
-  player2.style.height = "40px";
-  player2.style.width = "250px";
-  player2.style.margin = "10px";
-  player2.style.padding = "10px";
-  player2.style.borderRadius = "50px";
-  player2.style.fontSize = "20px";
+  player2.classList.add("playerInput");
+ 
   player2.setAttribute("placeholder", "Second Player");
 
   playerDiv.appendChild(player1);
@@ -133,13 +136,9 @@ function generateGameGrid() {
 
   let playerNameBtn = document.createElement("button");
   playerNameBtn.innerText = "Done";
-  playerNameBtn.style.width = "100px";
-  playerNameBtn.style.height = "40px";
-  playerNameBtn.style.marginTop = "30px";
-  playerNameBtn.style.borderRadius = "50px";
-  playerNameBtn.style.backgroundColor = "#61892F";
-  playerNameBtn.style.alignSelf = "center";
-  playerNameBtn.setAttribute("id", "playGame");
+  
+  playerNameBtn.classList.add("playerNameBtn");
+  
 
   playerNameBtn.addEventListener("mouseover", function changeColor() {
     playerNameBtn.style.backgroundColor = "skyblue";
@@ -165,14 +164,14 @@ function generateGameGrid() {
       for (let i = 0; i < 3; i++) {
         let row = document.createElement("div");
         row.style.display = "flex";
-        row.style.backgroundColor = "#61892F";
+        row.style.backgroundColor = "white";
         row.style.height = "100px";
         row.style.width = "300px";
         row.style.border = "1px solid black";
 
         for (let j = 0; j < 3; j++) {
           let column = document.createElement("div");
-          column.style.backgroundColor = "#61892F";
+          column.style.backgroundColor = "white";
           column.style.height = "100px";
           column.style.width = "300px";
           column.style.border = "1px solid black";
@@ -192,53 +191,55 @@ function generateGameGrid() {
             column.style.backgroundColor = "purple";
             column.style.cursor = "pointer";
             if (count % 2 == 0 && count <9) {
-              // namePlayer1.style.display="initial";
+                
               namePlayer1.style.visibility='visible';
               
               namePlayer1.innerText = player1.value + "  =====>   " + "X";
-              namePlayer1.style.marginBottom = "20px";
+              namePlayer1.style.marginBottom = "30px";
               namePlayer1.style.color = "black";
               console.log("inside If");
             } else if(count % 2 == 1 && count <9){
               // namePlayer2.style.display="initial";
               namePlayer1.style.visibility="visible";
               namePlayer1.innerText = player2.value + "  =====>   " + "0";
-              namePlayer1.style.marginBottom = "20px";
+              namePlayer1.style.marginBottom = "30px";
               namePlayer1.style.color = "black";
               console.log("inside else");
             }
           });
 
           column.addEventListener("mouseout", function () {
-            column.style.backgroundColor = "#61892F";
-            // namePlayer1.style.display="none";
-            // namePlayer2.style.display="none";
+            column.style.backgroundColor = "white";
+          
 
             namePlayer1.style.visibility="hidden";
-            // namePlayer2.style.visibility="hidden";
+           
           });
 
           row.appendChild(column);
 
-          //RESET FUNCTION
-          // function reset(){
-
-          // }
 
           //function to display X or 0 )
 
           column.addEventListener("click", function () {
             if (column.childNodes.length == 0) {
+
+              
               if (count % 2 == 0) {
+                correctAudio.play();
                 column.innerText = "X";
                 console.log("hello");
                 count++;
                 winGame();
               } else {
+                correctAudio.play();
                 column.innerText = "0";
                 count++;
                 winGame();
               }
+            }
+            else{
+              IncorrectAudio.play();
             }
           });
         }
